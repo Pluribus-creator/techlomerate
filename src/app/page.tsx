c9c13733a78320c757ed7411a2440f3851d66a87
Patrick Russell <sampson.patrick@gmail.com>
2026-06-05 06:04:29 -0700

    Adjust Apple ad crop position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default async function Home() {
             width: "100%",
             height: "120px",
             objectFit: "cover",
-            objectPosition: "center",
+            objectPosition: "center 30%",
             display: "block",
           }}
         />
